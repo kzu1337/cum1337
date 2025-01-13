b219b7ad6dd1739c4188b0a84a5a69b81a6b9b26
kzu1337 <francklampard15@gmail.com>
2025-01-13 22:44:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -466,6 +466,7 @@ const messages = {
     ],
     support: [
 
+	"If I told you I love surprises, what’s the one way you’d surprise me? 😏",    
 	"Do you ever wonder how tight my pussy feels, baby? 😈😈",
 	"I can’t stop thinking about your cock inside me, making me scream 😈🍆💋",
 	"What would you do if you had me bent over and begging for more? 😏🔥🍑",
